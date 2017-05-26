f079b6571a57d528a42de6cd553c984386f56dac
boyswan <jackwilliamboyce@gmail.com>
2017-05-26 16:22:54 +0100

    add blue

--- patterns.py
+++ patterns.py
@@ -2,30 +2,42 @@ import time
 
 
 def off(setLed):
+    # "OFF",
     setLed(0.0, 0.0, 0.0)
 
 
 def onTurquoise(setLed):
+    # "TUR",
     setLed(0.0, 255.0, 127.0)
 
 
 def onPink(setLed):
+    # "PIN",
     setLed(255.0, 20.0, 147.0)
 
 
 def onRed(setLed):
+    # "RED",
     setLed(255.0, 0.0, 0.0)
 
 
+def onBlue(setLed):
+    # "BLU",
+    setLed(0.0, 0.0, 255)
+
+
 def onGreen(setLed):
+    # "GRE",
     setLed(0.0, 255.0, 0.0)
 
 
 def onPurple(setLed):
+    # "PUR",
     setLed(138.0, 43.0, 226.0)
 
 
 def onYellow(setLed):
+    # "YEL"
     setLed(255.0, 255.0, 0.0)
 
 
@@ -51,4 +63,6 @@ def getPattern(pattern, setLed):
     elif (pattern == 'PUR'):
         onPurple(setLed)
     elif (pattern == 'YEL'):
+        onYellow(setLed)
+    elif (pattern == 'BLU'):
         onYellow(setLed)
